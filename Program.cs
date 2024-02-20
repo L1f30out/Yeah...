33ab7fa275bb125f5c94afdab126737cee4839c4
L1f30ut <voitcyberboy@gmail.com>
2024-02-20 08:02:56 +0200

    yeah

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello,Github!");
+        Console.WriteLine("My name is Daniel");
+        Console.WriteLine("I am 22 у.o. Idiot");
+        Console.WriteLine("And i have no clue what i am doing in my life");
+        Console.WriteLine("I am so lost");
+        Console.WriteLine("But i am not dead yet");
+        Console.WriteLine("So i randomly write this type of shit in 5am");
+        Console.WriteLine("and downloading it to my github");
+        Console.WriteLine("yeah...");
+    }
+}
